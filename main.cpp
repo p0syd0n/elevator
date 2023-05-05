45583a3f25a1783034fcca259cc08e112a89057d
posydon <114946695+p0syd0n@users.noreply.github.com>
2023-05-05 10:50:57 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,51 @@
+#pragma region VEXcode Generated Robot Configuration
+// Make sure all required headers are included.
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+#include <string.h>
+
+
+#include "vex.h"
+
+using namespace vex;
+
+// Brain should be defined by default
+brain Brain;
+
+
+// START V5 MACROS
+#define waitUntil(condition)                                                   \
+  do {                                                                         \
+    wait(5, msec);                                                             \
+  } while (!(condition))
+
+#define repeat(iterations)                                                     \
+  for (int iterator = 0; iterator < iterations; iterator++)
+// END V5 MACROS
+
+
+// Robot configuration code.
+motor Motor1 = motor(PORT1, ratio36_1, false);
+
+motor Motor2 = motor(PORT2, ratio36_1, false);
+
+limit LimitSwitch_Bottom_A = limit(Brain.ThreeWirePort.A);
+bumper Bumper_up1_H = bumper(Brain.ThreeWirePort.H);
+bumper Bumper_AllDown_B = bumper(Brain.ThreeWirePort.B);
+bumper Bumper_Floor3_C = bumper(Brain.ThreeWirePort.C);
+#pragma endregion VEXcode Generated Robot Configuration
+
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       {author}                                                  */
+/*    Created:      {date}                                                    */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
 // Include the V5 Library
 #include "vex.h"
   
@@ -12,24 +60,25 @@ void go_up() {
   Motor2.spinFor(forward, 9, turns, false);
 }
 
-void up_a_floor() {
-  switch (level) {
-    case 0:
-      Motor1.spinFor(reverse, 4, turns, false);
-      Motor2.spinFor(forward, 4, turns, false);
-  }
-  Motor1.spinFor(reverse, 4, turns, false);
-  Motor2.spinFor(forward, 4, turns, false);
-}
-
-void go_to_floor(int destination) {
-    Motor1.spinFor(reverse, 4*destination, turns, false);
-    Motor2.spinFor(forward, 4*destination, turns, false);
-}
-
-void up_a_floor1() {
+// void up_a_level() {
+//   switch (level) {
+//     case 0:
+//       Motor1.spinFor(reverse, 4, turns, false);
+//       Motor2.spinFor(forward, 4, turns, false);
+//   }
+//   Motor1.spinFor(reverse, 4, turns, false);
+//   Motor2.spinFor(forward, 4, turns, false);
+// }
+void up_a_level() {
     Motor1.spinFor(reverse, 4, turns, false);
     Motor2.spinFor(forward, 4, turns, false);
+    level+=1;
+}
+
+void down_a_level() {
+    Motor1.spinFor(forward, 4, turns, false);
+    Motor2.spinFor(reverse, 4, turns, false);
+    level+=1;
 }
 
 void go_bottom() {
@@ -43,6 +92,26 @@ void go_bottom() {
       break;
     }
   }
+}
+
+void go_to_level(int destination) {
+    if (destination == 0) {
+      go_bottom();
+    } else if (destination == 1) {
+      if (level == 0) {
+        up_a_level();
+      } else if (level == 2) {
+        down_a_level();
+      }
+    } else if (destination == 2) {
+      if (level == 1) {
+        up_a_level();
+      } else if (level == 0) {
+        for (int i = 0; i<2; i++) {
+          up_a_level();
+        }
+      }
+    }
 }
 
 void bottom_switch_callback() {
@@ -59,7 +128,7 @@ void up_a_turn() {
 }
 
 void test_floor() {
-  go_to_floor(2);
+  go_to_level(2);
 }
 
 void down_a_turn() {
@@ -73,8 +142,10 @@ void down_a_turn() {
 int main() {
   LimitSwitch_Bottom_A.pressed(bottom_switch_callback);
   LimitSwitch_Bottom_A.released(bottom_switch_callback_release);
-  Bumper_up1_B.pressed(up_a_floor1);
-  Bumper_AllDown_C.pressed(go_bottom);
+  Bumper_up1_H.pressed(up_a_level);
+  Bumper_AllDown_B.pressed(go_bottom);
+  //Bumper_Floor3_C.pressed(go_to_level(2))
   Motor1.setVelocity(70, percent);
   Motor2.setVelocity(70, percent);
+  go_bottom();
 }
